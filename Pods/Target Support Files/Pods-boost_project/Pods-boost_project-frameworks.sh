7ab4137e0333f9b940b87dc0aaee3213744ef2a0
ByoungHwi Yoon <1994blyo@gmail.com>
2020-08-07 22:09:15 +0900

    DB생성 및 키워드를 통한 DB탐색, 챗봇반응 구현

--- Pods/Target Support Files/Pods-boost_project/Pods-boost_project-frameworks.sh
+++ Pods/Target Support Files/Pods-boost_project/Pods-boost_project-frameworks.sh
@@ -197,10 +197,12 @@ if [ -r "${ARTIFACT_LIST_FILE}" ]; then
 fi
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
+  install_framework "${BUILT_PRODUCTS_DIR}/FMDB/FMDB.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/InputBarAccessoryView/InputBarAccessoryView.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/MessageKit/MessageKit.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
+  install_framework "${BUILT_PRODUCTS_DIR}/FMDB/FMDB.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/InputBarAccessoryView/InputBarAccessoryView.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/MessageKit/MessageKit.framework"
 fi
--- ViewController.swift
+++ ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: MessagesViewController, MessagesDataSource, MessagesLayout
     let currentUser = Sender(senderId: "self", displayName: "iOS Academy")
     let otherUser = Sender(senderId: "other", displayName: "John Smith")
     var messages = [MessageType]()
+    let db = DBManager.getInstance()
     
     func currentSender() -> SenderType {
         return currentUser
@@ -33,6 +34,9 @@ class ViewController: MessagesViewController, MessagesDataSource, MessagesLayout
         messagesCollectionView.messagesLayoutDelegate = self
         messagesCollectionView.messagesDisplayDelegate = self
         messageInputBar.sendButton.addTarget(self, action: #selector(self.sendAction(sender:)), for: .touchUpInside)
+        
+        messageInputBar.inputTextView.becomeFirstResponder()
+        
     }
     
     @IBAction func sendAction(sender: UIButton){
@@ -49,13 +53,75 @@ class ViewController: MessagesViewController, MessagesDataSource, MessagesLayout
          output : json 파싱 값
         */
         
+        let keywords = APIManager.getKeywords(from: inputData ?? "")
+        //var iidSet = Set<Int>()
+        var iidDic = [Int:Int]()
+        for keyword in keywords {
+            let items = db.findByKeyword(keyword: keyword)
+            
+            for item in items {
+                iidDic.updateValue((iidDic[item.iid] ?? 0) + 1, forKey: item.iid)
+                //iidSet.update(with: item.iid)
+            }
+        }
+        
+        let bestMatchID = getBestMatchs(iidDic)
+        
+       
+        var realEstateItems = [RealEstateItem]()
+        
+        for iid in bestMatchID {
+            let items = db.findByIID(iid: iid)
+            realEstateItems.append(contentsOf: items)
+        }
+        
+        for item in realEstateItems {
+            messages.append(Message(sender: otherUser,
+            messageId: "7",
+            sentDate: Date(),
+            kind: .text("\(item.address) ")))
+        }
+        
+        if realEstateItems.count == 0 {
+            messages.append(Message(sender: otherUser,
+            messageId: "6",
+            sentDate: Date(),
+            kind: .text("해당하는 매물을 찾을 수 없습니다.")))
+        }
+        
         //응답 메시지
-        messages.append(Message(sender: otherUser,
-        messageId: "6",
-        sentDate: Date(),
-        kind: .text("hi!")))
+        //messages.append(Message(sender: otherUser,
+        //messageId: "6",
+        //sentDate: Date(),
+        //kind: .text("hi!")))
         messageInputBar.inputTextView.text = ""
         messagesCollectionView.reloadData()
+        
     }
+    
+    func getBestMatchs(_ items : [Int:Int]) -> [Int] {
+        
+        if items.count == 0 {
+            return [Int]()
+        }
+        
+        let sortedItems = items.sorted { $0.1 > $1.1 }
+        
+        let bestMatchCount = sortedItems[0].value
+        
+        var bestMatchedIds = [Int]()
+        
+        for item in sortedItems {
+            if item.value == bestMatchCount{
+                bestMatchedIds.append(item.key)
+                continue
+            }
+            break
+        }
+        
+        return bestMatchedIds
+        
+    }
+    
 }
 
